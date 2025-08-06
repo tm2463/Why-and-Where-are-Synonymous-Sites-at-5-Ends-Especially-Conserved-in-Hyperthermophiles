fe98512c509f1afc24d41ab01052cab336ef22bc
tm2463 <tm2463@bath.ac.uk>
2025-08-06 20:35:01 +0100

    Update mafft.sh

--- phylogenetics/mafft.sh
+++ phylogenetics/mafft.sh
@@ -2,5 +2,9 @@
 
 for file in *.txt; do
 	gene=$(basename "$file" .txt)
-	mafft --bl 30 "file" > "${gene}_msa.txt"
+	mafft --bl 30 "$file" > "${gene}_msa.txt"
+	trimal -in "${gene}_msa.txt" -out "${gene}_trimal.txt"
 done
+
+mkdir -p cleaned_alignment
+mv *_trimal.txt cleaned_alignment
