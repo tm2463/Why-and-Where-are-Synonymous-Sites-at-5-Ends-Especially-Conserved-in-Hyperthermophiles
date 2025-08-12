7d0967af16ecc79a0a86204c95093dc0419df38c
tm2463 <tm2463@bath.ac.uk>
2025-08-12 14:21:29 +0100

    Update perm_graph.r

--- permutation_analysis/perm_graph.r
+++ permutation_analysis/perm_graph.r
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(wesanderson)
 
 meso <- read.csv('meso.csv', sep=',', header=FALSE)
 thermo <- read.csv('thermo.csv', sep=',', header=FALSE)
@@ -19,12 +20,17 @@ meso$Window <- windows
 thermo$Window <- windows
 hyper$Window <- windows
 
-plot <- bind_rows(meso, thermo, hyper)
+combined <- bind_rows(meso, thermo, hyper)
+combined$Group <- factor(combined$Group, levels = c("Mesophiles", "Thermophiles", "Hyperthermophiles"))
 
-graph <- ggplot(data=plot, aes(x=Window, y=Z, group=Group, color=Group)) +
+pal <- wes_palette('Darjeeling1', 4)
+pal <- pal[c(2, 4, 1)]
+
+graph <- ggplot(data=combined, aes(x=Window, y=Z, group=Group, color=Group, shape=Group)) +
 	geom_errorbar(aes(ymin=Z-sem, ymax=Z+sem), width=1.5) +
 	geom_line() + geom_point(size=3) +
 	scale_x_continuous(breaks = c(-30, -15, 0, 15, 30)) +
-	labs(title='Local RNA Folding Energy (OGT)', y='Z-Score', x='Window Start (nt)')
+	scale_color_manual(values=pal) +
+	labs(title='Local RNA Folding Energy (37°C)', y='Z-Score', x='Window Start (nt)')
 
-ggsave('perm_ogt.png', plot=graph, width=6, height=4)
+ggsave('perm_37.png', plot=graph, width=6, height=4)
