642b2975cacf07555c7c96201e2cd0c4b5e3e4cd
tm2463 <tm2463@bath.ac.uk>
2025-08-12 15:00:00 +0100

    Update box_plot.r

--- permutation_analysis/box_plot.r
+++ permutation_analysis/box_plot.r
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(wesanderson)
 
 meso <- read.csv('meso.csv', sep=',', header=FALSE)
 thermo <- read.csv('thermo.csv', sep=',', header=FALSE)
@@ -9,11 +10,16 @@ meso$Group <- "Mesophiles"
 thermo$Group <- "Thermophiles"
 hyper$Group <- "Hyperthermophiles"
 
-plot <- bind_rows(meso, thermo, hyper)
+combined <- bind_rows(meso, thermo, hyper)
+combined$Group <- factor(combined$Group, levels = c("Mesophiles", "Thermophiles", "Hyperthermophiles"))
 
-box <- ggplot(plot, aes(x=Group, y=V1, fill=Group)) +
+pal <- wes_palette('Darjeeling1', 4)
+pal <- pal[c(2, 4, 1)]
+
+box <- ggplot(combined, aes(x=Group, y=V1, fill=Group)) +
 	geom_boxplot() +
 	geom_hline(yintercept = 0) +
+	scale_fill_manual(values=pal) +
 	labs(title='Start Codon Accessibility', x=NULL, y='Δ Proportion of Unpaired Bases')
 
 ggsave('box.png', plot=box, width=6, height=4)
